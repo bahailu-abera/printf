9a9423f5d35a4598288d9482ab8d492ec1e14602
bahailu-abera <aberabahailu4@gmail.com>
2022-04-18 23:57:46 -0700

    handle %b

--- print_bin.c
+++ print_bin.c
@@ -12,4 +12,27 @@
 
 int print_bin(va_list args, char *buffer)
 {
+	unsigned int num;
+	unsigned int i = 0, j, temp;
+
+	free(buffer);
+	buffer = malloc(sizeof(char) * 10);
+
+	num = va_arg(args, int);
+
+	while (num > 0)
+	{
+		buffer[i] = (num % 2) + '0';
+		num /= 2;
+		i++;
+	}
+
+	for (j = 0, i++; j < i / 2; j++)
+	{
+		temp = buffer[j];
+		buffer[j] = buffer[i - (j + 1)];
+		buffer[i - (j + 1)] = temp;
+	}
+	return (_print_buf(buffer, i));
+
 }
